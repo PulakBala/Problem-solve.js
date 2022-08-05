d2b09d6849de0a15451db628e486af13a974075e
PulakBala <pulakbala0524@gmail.com>
2022-08-05 18:33:44 +0600

    road crossing problem solve

---
+++ problem-10.js
@@ -0,0 +1,14 @@
+const signal = "green";
+switch (signal) {
+    case 'red':
+        console.log('red signal e jodi hato taile capa pore jabe ');
+        break;
+    case 'yellow':
+        console.log('yellow coloured traffic signal , you should stop');
+        break;
+    case 'green':
+        console.log('If you notice a green coloured, you should cross the road')
+        break;
+    default :
+        console.log('bristi namse bari cole ay')            
+} 
---
+++ problem-9.js
@@ -0,0 +1,17 @@
+function odd_even(num) {
+     return num % 2 === 0? 'odd' : 'even';
+}
+console.log(odd_even(12))
+
+// function odd(num) {
+//      var  mark = num % 2 == 0;
+//      return mark;
+// }
+// function even(num) {
+//     var mark = num % 2 ==1;
+//     return mark;
+// }
+
+// console.log(odd(12), even(12));
+
+
