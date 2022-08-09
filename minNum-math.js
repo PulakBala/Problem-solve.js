c12a84e8f46a98c0abcd3bc010ddee43a0584202
PulakBala <pulakbala0524@gmail.com>
2022-08-09 14:44:12 +0600

    find min number

---
+++ minNum-math.js
@@ -0,0 +1,7 @@
+//write a function that will take 3 numbers wil return the min number . use math.min;
+
+
+function maxNumber (numbers) {
+    return numbers = Math.min(13, 21, 12);
+}
+console.log(maxNumber());
