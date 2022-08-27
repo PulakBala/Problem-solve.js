ae60d9e217548b2f4c9b2322d3b31d64981dda72
PulakBala <pulakbala0524@gmail.com>
2022-08-27 12:19:51 +0600

    default arrow function

--- Es6-Problem-solve/backticks.js
+++ Es6-Problem-solve/backticks.js
@@ -1,3 +1,4 @@
+// write the following sentence in three lines and print
 const threeLines = `I am a web developer.
 I love to code.
 I love to eat biryani.`
---
+++ Es6-Problem-solve/default-value.js
@@ -0,0 +1,3 @@
+//write a arrow function that will take 2 parameters: one parameter will come from you and the other parameter will be a default parameter . Add these two parameters and return the result.
+const add = (num1 , num2 = 39) => num1 + num2;
+console.log(add(10));
