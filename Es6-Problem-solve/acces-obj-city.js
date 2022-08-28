005faf383c3b2187db4d11614b15c410b0e6b7d1
PulakBala <pulakbala0524@gmail.com>
2022-08-29 01:54:22 +0600

    acces object city

---
+++ Es6-Problem-solve/acces-obj-city.js
@@ -0,0 +1,13 @@
+//access object city
+
+let data = {
+    location: [
+        {
+            latitude: '34.5, 37.8',
+            longitude: '98, 77, 58,7',
+            city : 'Hyderabad',
+            country: 'India'
+        }
+    ]
+};
+console.log(data.location[0].city);
