f1cb7c47361b86cd2a4e5b0e5157a9c006b15986
PulakBala <pulakbala0524@gmail.com>
2022-09-09 13:43:23 +0600

    add confirm

---
+++ browser/confirm/main.js
@@ -0,0 +1,10 @@
+
+/*
+ব্রাউজারে একটা confirm করে জিজ্ঞেস করো তুমি কি তোমার ওয়েবসাইট এর লোকেশন দেখতে চাও। যদি বলে দেখতে চাই তাহলে সেই ওয়েবসাইট এর লোকেশন এর href জিনিসটা কনসোল লগ করে দেখাও 
+*/
+const cinfirmField = ()=> {
+    confirm('You want your current website location?')
+    if(confirm = true) {
+        console.log(window.location.href);
+    }
+}
